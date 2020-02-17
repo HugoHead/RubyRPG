7168291b20980105021159e86181b316e18f9e77
HugoHead <45836267+HugoHead@users.noreply.github.com>
2020-02-17 08:33:42 -0500

    Added some changes

--- main.rb
+++ main.rb
@@ -1,4 +1,51 @@
-puts "Welcome to a text adventure!"
-puts "What is your name?"
-name = gets.chomp("\n")
-puts "Hello #{name}."
+def select (options)
+	i = 1
+	options.each do |n|
+		puts i.to_s + ") " + n
+		i += 1
+	end
+	puts "\n"
+
+	resposnse = gets.chomp("\n")
+
+	isInt = false
+
+	i.times do |m|
+		if resposnse.to_i == i
+			isInt = true
+			break
+		end
+	end
+
+	puts (isInt)
+
+	if isInt
+		return options[resposnse]
+	else
+		return select(options)
+	end
+end
+
+class Player
+	attr_accessor :archi, :name,	
+
+	def determineArchi
+		return select(["rouge", "mage", "brute"])
+	end
+
+	def initialize
+		puts "Welcome to a text adventure!"
+		puts "What is your name?"
+		name = gets.chomp("\n")
+		self.name = name
+		puts "Hello #{self.name}."
+
+		self.archi = determineArchi
+
+		puts self.archi
+	end
+end
+
+$user = Player.new
+
+puts select(["rouge", "mage", "brute"])
