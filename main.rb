bc678a2763ad1e1e5d454b71c81feabf3106f599
HugoHead <45836267+HugoHead@users.noreply.github.com>
2020-05-07 14:30:06 -0400

    Add files via upload

--- main.rb
+++ main.rb
@@ -5,6 +5,10 @@ def pause
 	gets
 end
 
+def look
+	puts $chalange.description.green
+end
+
 def run 
 	if $user.currentLevel == 0
 		puts "You turn to run. As you turn you see a massive brick wall that has been summond by the rival. You turn back to see him grinning."
@@ -12,7 +16,25 @@ def run
 end
 
 def punch
-
+	print "You attempt to punch the ".green + $chalange.amnimateEnemyName.green + ". ".green
+	punchStrength = $user.energy * rand()
+	punchDifficulty = $chalange.energy * rand()
+	if punchStrength / 2 > punchDifficulty
+		#critical hit
+		print "You hit really hard.\n".green
+		$chalange.energy -= punchStrength * 1.25
+	elsif punchStrength > punchDifficulty
+		#hit
+		print "You hit.\n".green
+		$chalange.energy -= punchStrength
+	elsif punchStrength * 2 > punchDifficulty
+		#no hit
+		print "You missed.\n".green
+	else
+		#mistep
+		print "You trip and fall.\n".green
+		$user.energy -= punchDifficulty * 0.25
+	end
 end 
 
 def cast
@@ -21,7 +43,7 @@ end
 
 def sneak
 	arr = ["to", "away"]
-	select "Sneak toards or away from the situation?"
+	select "Sneak toards or away from the situation?", arr
 end
 
 def lift
@@ -29,12 +51,53 @@ def lift
 end
 
 def persuade
+	amnimateEnemyName = $chalange.amnimateEnemyName
+	arr = ["deeculate conflict", "desctract " + $chalange.amnimateEnemyName, "attempt to end conflict", "recive money from " + amnimateEnemyName, "anger " + amnimateEnemyName]
+	goal = select "With which goal?", arr
+	arr = ["clever words", "threat", "bribe"]
+	techneque = select "With what method?", arr
+	succsess = false
+	if (goal == "recive money from " + amnimateEnemyName && techneque == "bribe") || (goal == "deeculate conflict" && techneque == "clever words") || (goal == "attempt to end conflict" && techneque == "clever words")
+		#super effective
+		succsess = (rand() > 0.4)
+	elsif (goal == "desctract " + amnimateEnemyName && techneque == "clever words") || (goal == "anger " + amnimateEnemyName && techneque == "clever words") || (goal == "anger " + amnimateEnemyName && techneque == "threat")
+		#effective
+		succsess = (rand() > 0.65)
+	elsif (goal == "recive money from " + amnimateEnemyName && techneque == "clever words") || (goal == "desctract " + amnimateEnemyName && techneque == "bribe") || (goal == "attempt to end conflict" && techneque == "threat")
+		#nutreal
+		succsess = (rand() > 0.8)
+	elsif (goal == "recive money from " + amnimateEnemyName && techneque == "threat") || (goal == "desctract " + amnimateEnemyName && techneque == "threat") || (goal == "attempt to end conflict" && techneque == "bribe")
+		#under effective
+		succsess = (rand() > 0.89)
+	else 
+		#useless
+		succsess = false
+	end
 
+	if succsess
+		if goal == "deeculate conflict"
+			puts "the conflict was desculated"
+		elsif goal == "desctract " + $chalange.amnimateEnemyName
+			puts $chalange.amnimateEnemyName + " is desctracted"
+		elsif goal == "attempt to end conflict"
+			puts "conflict ended"
+			changeLevel(1)
+		elsif goal == "recive money from " + amnimateEnemyName
+			puts "you got some money"
+		elsif goal == "anger " + amnimateEnemyName
+			puts amnimateEnemyName + " is now more angry"
+		end
+	else
+		puts "you fail"
+	end
+			
 end
 
 def handle(option)
 	if option == "run"
 		run
+	elsif option == "look"
+		look
 	elsif option == "punch"
 		punch
 	elsif option == "cast"
@@ -82,28 +145,33 @@ def select (name, options)
 end
 
 def changeLevel (amm)
+	puts "level changing"
 	level = $user.currentLevel
 	level += amm
 
 				     #name,          threatname,      chalangeLevel,      locationName,      description
 	levelZero = 	
 		Chalange.new "rival enemy",  "rival",         1,                  "caveEntrance",      "You see a cave. The cave contians a rival of yours."
+	levelOne = 	
+		Chalange.new "rival enemy",  "rival",         2,                  "caveEntrance",      "You see a cave. The cave contians a rival of yours."
 
 	levels = [
 		levelZero
+		levelOne
 	]
 	$user.currentLevel += amm
 	return levels[level]
 end
 
 class Chalange
-	attr_accessor :nAme, :amnimateEnemyName, :location, :chalangeLevel, :description
+	attr_accessor :nAme, :amnimateEnemyName, :location, :chalangeLevel, :description, :energy
 	def initialize (nAme, amnimateEnemyName, chalangeLevel, location, description)
 		self.nAme = nAme
 		self.amnimateEnemyName = amnimateEnemyName
 		self.chalangeLevel = chalangeLevel
 		self.location = location
 		self.description = description
+		self.energy = 100 * (chalangeLevel + 1) 
 	end
 end
 
@@ -113,7 +181,7 @@ class Player
 	def initialize
 		self.energy = 100
 		self.currentLevel = 0
-		self.availableOptions  = ["run", "punch", "persuade", "cast"]	
+		self.availableOptions  = ["look", "run", "punch", "persuade", "cast"]	
 		puts "Welcome to a text adventure!".green
 		puts "What is your name?".green
 		name = gets.chomp("\n")
@@ -128,10 +196,24 @@ $chalange = changeLevel(0)
 
 gameRunning = true
 
+staggingLevel = $chalange.chalangeLevel - 1
+
 while gameRunning == true
-	puts $chalange.description.green
+	#only continue if the threat is still in place
+	if $chalange.energy <= 0
+		puts "you moved to the next level."
+		$chalange = chalangeLevel(1)
+	end
+
+	#check if the description for the current level has been displayed yet
+	if staggingLevel != $chalange.chalangeLevel
+		puts $chalange.description.green
+		staggingLevel = $chalange.chalangeLevel
+	end
+
+
 	pause
 	option = select("Pick an option", $user.availableOptions)
 	pause
 	handle(option)
-end
+ended
